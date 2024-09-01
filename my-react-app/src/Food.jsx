da24d4e41e24b2aab74ace3120e86099b0d46b2e
Czkafek <czkafek@gmail.com>
2024-09-01 23:00:44 +0200

    first components in react no way

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,19 @@
+
+function Food() {
+    
+    const food1 = "orange";
+    const food2 = "banana";
+    const food3 = "apple";
+
+    
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>{food1}</li>
+            <li>{food2.toUpperCase()}</li>
+            <li>{food3}</li>
+        </ul>
+    );
+}
+
+export default Food;
